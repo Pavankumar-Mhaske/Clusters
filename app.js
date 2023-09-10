9525aa11e889ceb7a20a63a794393eacea2b92dc
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-09-10 15:35:17 +0530

    server setup .......

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+const PORT = 8000;
+
+app.get("/", (req, res) => {
+  return res.json({
+    message: "hellow from Express Server  🚀",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port http://localhost:${PORT}`);
+});
